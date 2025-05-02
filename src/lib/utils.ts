3cf5c281b7da898557b8e8c4fe89af289d8916ab
nicorosales <nirogo06@gmail.com>
2025-05-02 10:22:47 +0200

    update fix ports railway

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const BASE_URL = "https://my-app.up.railway.app";
+
+export default BASE_URL;
